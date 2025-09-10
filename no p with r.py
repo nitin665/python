8ecab47b3771369c16f426240a18f4a5479793e4
nitin665 <nitu1142008@gmail.com>
2025-09-10 18:17:21 +0530

    Add files via upload

---
+++ no p with r.py
@@ -0,0 +1,7 @@
+def add():
+    a=int(input("first num"))
+    b=int(input("second num"))
+    s=a+b
+    return s
+    
+print(add())
---
+++ no parameter and return.py
@@ -0,0 +1,5 @@
+def add():
+    a=int(input("first num"))
+    b=int(input("second num"))
+    print("sum",a+b)
+add()
